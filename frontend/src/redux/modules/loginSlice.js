39af2260e9ac4ce6a0f452a096874cf37b5e1340
grace <gracehoneybee740@gmail.com>
2023-01-30 03:41:44 +0900

    [🚨]

--- frontend/src/redux/modules/loginSlice.js
+++ frontend/src/redux/modules/loginSlice.js
@@ -2,6 +2,8 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { api } from "../../shared/api";
 
+axios.defaults.withCredentials = true;
+
 const initialState = {
   user: {},
   isLoading: false,
@@ -12,14 +14,69 @@ const initialState = {
 export const __loginUser = createAsyncThunk(
   "loginUser",
   async (payload, thunkAPI) => {
-    try {
-      const data = await api.post(`auth/local`, payload);
-      console.log("login data", data);
-      return thunkAPI.fulfillWithValue(data);
-    } catch (error) {
-      alert("다시 확인해주세요.");
-      return thunkAPI.rejectWithValue(error);
-    }
+    //기존 코드
+    // try {
+    //   const data = await api.post(`auth/local`, payload);
+    //   console.log("login data", data);
+    //   return thunkAPI.fulfillWithValue(data);
+    // } catch (error) {
+    //   alert("다시 확인해주세요.");
+    //   return thunkAPI.rejectWithValue(error);
+    // }
+
+    //요청 인터셉터
+    api.interceptors.request.use(
+      function (config) {
+        //요청이 전달 되기 전에 작업 수행
+        const token = localStorage.getItem("token");
+        const refreshToken = localStorage.getItem("refreshToken");
+        try {
+          if (token && refreshToken) {
+            config.headers.authorization = token;
+            config.headers.refreshauthorization = refreshToken;
+          }
+          console.log("요청 성공! ", config);
+          return config;
+        } catch (error) {
+          console.log("에러! ", error);
+        }
+        return config;
+      },
+      function (error) {
+        //요청 오류가 있는 작업 수행
+        return Promise.reject(error);
+      }
+    );
+
+    //응답 인터셉터
+    api.interceptors.response.use(
+      function (response) {
+        console.log("응답 성공! ", response);
+        //로그인일 경우엔 토큰 세팅 필요
+        //응답 데이터가 있는 작업수행
+        if (response.status === 201) {
+          console.log("😂😂😂로그인 res.status : ", response);
+          const accessToken = response.data.accessToken;
+          const refreshToken = response.data.refreshToken;
+          localStorage.setItem("token", accessToken);
+          localStorage.setItem("refreshToken", refreshToken);
+          alert("로그인 성공!!!");
+          window.location.assign("/main");
+        }
+        //그러면 헤더에 토큰 값이 있을 경우를 로그인으로 처리?
+        //or 인증 전용 api를 따로 제작?
+        return response;
+      },
+      function (error) {
+        //응답 오류가 있는 작업 수행
+        console.log("응답 에러! ", error);
+        if (error.response.status > 401) {
+          alert("error!🔥");
+          window.location.assign("/");
+        }
+        return Promise.reject(error);
+      }
+    );
   }
 );
 
--- frontend/src/shared/api.js
+++ frontend/src/shared/api.js
@@ -12,57 +12,69 @@ export const api = axios.create({
   },
   //기본 헤더에는 토큰을 추가?
   //로그인 경우에는 추가할 토큰이 없다
-  //헤더 컨텐츠 타입이 jsom이 아니라 fromdata일 경우는 api를 따로 제작?
+  //헤더 컨텐츠 타입이 json이 아니라 fromdata일 경우는 api를 따로 제작?
 });
 
-api.interceptors.request.use(
-  function (config) {
-    const token = localStorage.getItem("token");
-    const refreshToken = localStorage.getItem("refreshToken");
-    try {
-      if (token && refreshToken) {
-        config.headers.authorization = token;
-        config.headers.refreshauthorization = refreshToken;
-      }
-      console.log("요청 성공! ", config);
-      return config;
-    } catch (error) {
-      console.log("에러! ", error);
-    }
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
-);
+api.interceptors.request.use(function (config) {
+  // console.log("들어가나");
 
-api.interceptors.response.use(
-  function (response) {
-    console.log("응답 성공! ", response);
-    //로그인일 경우엔 토큰 세팅 필요
-    if (response.status === 201) {
-      console.log("😂😂😂로그인 res.status : ", response);
-      const accessToken = response.data.accessToken;
-      const refreshToken = response.data.refreshToken;
-      localStorage.setItem("token", accessToken);
-      localStorage.setItem("refreshToken", refreshToken);
-      alert("로그인 성공!!!");
-      window.location.assign("/main");
-    }
-    //그러면 헤더에 토큰 값이 있을 경우를 로그인으로 처리?
-    //or 인증 전용 api를 따로 제작?
-    return response;
-  },
-
-  function (error) {
-    console.log("응답 에러! ", error);
-    if (error.response.status > 401) {
-      alert("error!🔥");
-      window.location.assign("/");
-    }
-    return Promise.reject(error);
+  const access_token = sessionStorage.getItem("access_token");
+  if (access_token !== null) {
+    // console.log(access_token);
+    config.headers.common["Authorization"] = `Bearer ${access_token}`;
   }
-);
+  return config;
+});
+
+//이전코드
+// api.interceptors.request.use(
+//   function (config) {
+//     const token = localStorage.getItem("token");
+//     const refreshToken = localStorage.getItem("refreshToken");
+//     try {
+//       if (token && refreshToken) {
+//         config.headers.authorization = token;
+//         config.headers.refreshauthorization = refreshToken;
+//       }
+//       console.log("요청 성공! ", config);
+//       return config;
+//     } catch (error) {
+//       console.log("에러! ", error);
+//     }
+//     return config;
+//   },
+//   function (error) {
+//     return Promise.reject(error);
+//   }
+// );
+
+// api.interceptors.response.use(
+//   function (response) {
+//     console.log("응답 성공! ", response);
+//     //로그인일 경우엔 토큰 세팅 필요
+//     if (response.status === 201) {
+//       console.log("😂😂😂로그인 res.status : ", response);
+//       const accessToken = response.data.accessToken;
+//       const refreshToken = response.data.refreshToken;
+//       localStorage.setItem("token", accessToken);
+//       localStorage.setItem("refreshToken", refreshToken);
+//       alert("로그인 성공!!!");
+//       window.location.assign("/main");
+//     }
+//     //그러면 헤더에 토큰 값이 있을 경우를 로그인으로 처리?
+//     //or 인증 전용 api를 따로 제작?
+//     return response;
+//   },
+
+//   function (error) {
+//     console.log("응답 에러! ", error);
+//     if (error.response.status > 401) {
+//       alert("error!🔥");
+//       window.location.assign("/");
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 // 생성한 인스턴스를 익스포트 합니다.
 export default api;
