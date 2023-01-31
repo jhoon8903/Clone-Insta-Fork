b1978f66ca249016e71a0ef459888eccd0e8eddc
F1rstID <wndhdks4536@gmail.com>
2023-01-31 22:26:49 +0900

    [BE][🚑] : hotfix Module Imports

--- backend/src/dms/dms.module.ts
+++ backend/src/dms/dms.module.ts
@@ -1,10 +1,18 @@
+import { DMEntity } from './dms.entity';
+import { UserEntity } from 'src/Users/users.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/Users/users.module';
 import { EventsModule } from './../events/events.module';
 import { Module } from '@nestjs/common';
 import { DmsController } from './dms.controller';
 import { DmsService } from './dms.service';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    EventsModule,
+    UsersModule,
+    TypeOrmModule.forFeature([UserEntity, DMEntity]),
+  ],
   controllers: [DmsController],
   providers: [DmsService],
 })
